334e09797851da93260f947d36d77be7c4c16e0c
Arpan Mistry <79263668+Arpan-Mistry@users.noreply.github.com>
2022-09-24 16:20:13 +0530

    Time: 3 ms (5.37%), Space: 46.8 MB (78.04%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -3,42 +3,41 @@ class Solution {
         int s = 0;
         int e = nums.length - 1;
         int[] arr = { -1, -1 };
+        if (nums.length == 0 || target < nums[s] || target > nums[e]) {
+            return arr;
+        }
         int i;
         int j;
         int pos = -1;
-        if (nums.length == 0 || target < nums[s] || target > nums[e]) {
-            return arr;
-        } else {
-            while (s <= e) {
-                int mid = (s + e) / 2;
-                if (target >= nums[mid]) {
-                    if (target == nums[mid] && pos == -1) {
-                        pos = mid;
-                        System.out.println(pos);
-                    }
-                    s = mid + 1;
-                } else if (target < nums[mid]) {
-                    e = mid - 1;
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (target >= nums[mid]) {
+                if (target == nums[mid] && pos == -1) {
+                    pos = mid;
+                    System.out.println(pos);
                 }
+                s = mid + 1;
+            } else if (target < nums[mid]) {
+                e = mid - 1;
             }
+        }
 
-            j = e;
-            s = 0;
-            e = pos - 1;
+        j = e;
+        s = 0;
+        e = pos - 1;
 
-            while (s <= e) {
-                int mid = (s + e) / 2;
-                if (target > nums[mid]) {
-                    s = mid + 1;
-                } else if (target <= nums[mid]) {
-                    e = mid - 1;
-                }
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (target > nums[mid]) {
+                s = mid + 1;
+            } else if (target <= nums[mid]) {
+                e = mid - 1;
             }
-            i = s;
-            if (pos != -1) {
-                arr[0] = i;
-                arr[1] = j;
-            }
+        }
+        i = s;
+        if (pos != -1) {
+            arr[0] = i;
+            arr[1] = j;
         }
         return arr;
 
