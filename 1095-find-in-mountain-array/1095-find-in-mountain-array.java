84fe41c1729ceddc6b82a11de4fc230f1d33cd62
Arpan Mistry <79263668+Arpan-Mistry@users.noreply.github.com>
2022-09-25 16:17:17 +0530

    Time: 19 ms (5.48%), Space: 44.2 MB (14.35%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,60 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findTarget(int target,MountainArray arr,int s,int e,boolean asc){
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(arr.get(mid)<target){
+                if(asc) s=mid+1;
+                else {e=mid-1;
+                     System.out.println("left");
+                     }
+            }
+            else if(arr.get(mid)>target){
+                if(asc) e=mid-1;
+                else {s=mid+1;System.out.println("right");}
+            }
+            else return mid;
+        }
+        return -1;
+    }
+    
+    
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int s=0;
+        int e = mountainArr.length()-1;
+        int peak=-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            int mid_val = mountainArr.get(mid);
+            int mid_r_val = mountainArr.get(mid+1);
+            if(mid==0 && (mid_r_val>mid_val)){
+               peak=mid+1;
+                break;
+            }
+            int mid_l_val = mountainArr.get(mid-1);
+            if(mid_r_val<mid_val && mid_l_val<mid_val){
+            peak=mid;
+            break;   
+            }
+            else if((mid_l_val < mid_val) && (mid_r_val > mid_val)) s=mid+1;
+            else e=mid-1;
+        }
+        int left = findTarget(target,mountainArr,0,peak,true);
+        int right = findTarget(target,mountainArr,peak+1,mountainArr.length()-1,false);
+        System.out.println("peak--->"+peak+"\nleft--->"+left+"\nright--->"+right);
+        if(left==-1||right==-1){
+            return (left<right)?right:left;
+        }
+        else if(left<right) return left;
+        else if(left>right) return right;
+        return -1;
+    }
+}
