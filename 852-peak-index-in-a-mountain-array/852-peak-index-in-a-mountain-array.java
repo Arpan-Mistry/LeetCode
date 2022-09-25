a670299a000ec37e89998fc16ec08d33bd4e30a7
Arpan Mistry <79263668+Arpan-Mistry@users.noreply.github.com>
2022-09-25 11:36:58 +0530

    Time: 0 ms (100.00%), Space: 57.1 MB (99.84%) - LeetHub

---
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int s=0;
+        int e=arr.length-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if((mid==0 && arr[mid]<arr[mid+1])||(mid == arr.length-1 && arr[mid-1]>arr[mid])){
+                return mid+1;
+            }
+            else if(arr[mid-1]<arr[mid] && arr[mid+1]>arr[mid]){
+                s=mid+1;
+            }
+            else if(arr[mid-1]>arr[mid] && arr[mid+1]<arr[mid]){
+                e=mid-1;
+            }
+            else{
+               return mid; 
+            }
+        }
+        return -1;
+    }
+}
