cd8b034579680952cffd1c30a86f509ca4f1f6c0
Arpan Mistry <79263668+Arpan-Mistry@users.noreply.github.com>
2022-10-10 12:37:10 +0530

    Time: 1 ms (84.68%), Space: 51.6 MB (21.00%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,16 +1,33 @@
 class Solution {
+    // public int missingNumber(int[] nums) {
+    //     HashSet<Integer> set=new HashSet<>();
+    //     int len_nums=nums.length;
+    //     for(int x:nums){
+    //         set.add(x);
+    //     }
+    //     for(int i=0;i<len_nums;i++){
+    //         if(!set.contains(i)){
+    //             return i;
+    //         }
+    //     }
+    //     return len_nums;
+    // }
     public int missingNumber(int[] nums) {
-        HashSet<Integer> set=new HashSet<>();
-        int len_nums=nums.length;
-        for(int x:nums){
-            set.add(x);
-        }
-        for(int i=0;i<len_nums;i++){
-            if(!set.contains(i)){
-                return i;
+        for(int i=0;i<nums.length;i++){
+            while(nums[i]!=i){
+                if(nums[i]<nums.length){
+                    int temp = nums[nums[i]];
+                    nums[nums[i]] = nums[i];
+                    nums[i]=temp;
+                }
+                else{
+                    break;
+                }
             }
         }
-        return len_nums;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=i) return i;
+        }
+        return nums[nums.length-1]+1;
     }
 }
-    
