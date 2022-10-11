059d0b8f57bf5d0199815a17f73755e24d283172
Arpan Mistry <79263668+Arpan-Mistry@users.noreply.github.com>
2022-10-11 21:42:05 +0530

    Time: 15 ms (27.54%), Space: 54.4 MB (64.00%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,14 +1,26 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            while(nums[i]!=i+1){
-                if(nums[nums[i]-1]==nums[i]) break;
-                int temp=nums[nums[i]-1];
-                nums[nums[i]-1]=nums[i];
-                nums[i]=temp;
+        // for(int i=0;i<nums.length;i++){
+        //     while(nums[i]!=i+1){
+        //         if(nums[nums[i]-1]==nums[i]) break;
+        //         int temp=nums[nums[i]-1];
+        //         nums[nums[i]-1]=nums[i];
+        //         nums[i]=temp;
+        //     }
+        // }
+        int i=0;
+        while(i<nums.length){
+            if(nums[i]!=i+1){
+                if(nums[nums[i]-1]!=nums[i]){
+                     int temp=nums[nums[i]-1];
+                     nums[nums[i]-1]=nums[i];
+                     nums[i]=temp; 
+                }
+                else i++;   
             }
+            else i++;
         }
-        for(int i=0;i<nums.length;i++){
+        for(i=0;i<nums.length;i++){
             if(nums[i]!=i+1) return new int[] {nums[i],i+1};
         }
         return new int[] {-1,-1};
