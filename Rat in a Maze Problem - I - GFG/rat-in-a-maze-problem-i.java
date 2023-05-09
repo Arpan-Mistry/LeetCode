b1a3053be7d7a81a64c46d5d98144f6142f0faa9
Arpan Mistry <79263668+Arpan-Mistry@users.noreply.github.com>
2023-05-09 18:36:14 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.java
@@ -34,51 +34,48 @@ class Rat {
 
 // m is the given matrix and n is the order of matrix
 class Solution {
-  private static void solve(int i, int j, int a[][], int n, ArrayList < String > ans, String move,
-    int vis[][]) {
-    if (i == n - 1 && j == n - 1) {
-      ans.add(move);
-      return;
+    
+    public static void run(int[][] arr,int n,int i,int j,String move,ArrayList<String> out,int[][] vis){
+        if(i==n-1 && j==n-1){
+            out.add(move);
+            return;
+        }
+        
+        if(i+1<n && vis[i+1][j]==0 && arr[i+1][j]==1){
+            vis[i][j]=1;
+            run(arr,n,i+1,j,move+'D',out,vis);
+            vis[i][j]=0;
+        }
+        
+        if(j - 1 >= 0 && vis[i][j - 1] == 0 && arr[i][j - 1] == 1){
+            vis[i][j]=1;
+            run(arr,n,i,j-1,move+'L',out,vis);
+            vis[i][j]=0;
+        }
+        
+        if(j+1 < n && vis[i][j+1]==0 && arr[i][j+1]==1){
+            vis[i][j]=1;
+            run(arr,n,i,j+1,move+'R',out,vis);
+            vis[i][j]=0;
+        }
+        
+        
+        if(i-1 >= 0 && vis[i-1][j] == 0 && arr[i-1][j]==1){
+            vis[i][j]=1;
+            run(arr,n,i-1,j,move+'U',out,vis);
+            vis[i][j]=0;
+        }
     }
-
-    // downward
-    if (i + 1 < n && vis[i + 1][j] == 0 && a[i + 1][j] == 1) {
-      vis[i][j] = 1;
-      solve(i + 1, j, a, n, ans, move + 'D', vis);
-      vis[i][j] = 0;
+    
+    public static ArrayList<String> findPath(int[][] arr, int n) {
+        ArrayList<String> out = new ArrayList<>();
+        int[][] vis=new int[n][n];
+        for(int x=0;x<vis.length;x++){
+            for(int y=0;y<vis.length;y++){
+                vis[x][y]=0;
+            }
+        }
+        if (arr[0][0] == 1) run(arr,n,0,0,"",out,vis);
+        return out;
     }
-
-    // left
-    if (j - 1 >= 0 && vis[i][j - 1] == 0 && a[i][j - 1] == 1) {
-      vis[i][j] = 1;
-      solve(i, j - 1, a, n, ans, move + 'L', vis);
-      vis[i][j] = 0;
-    }
-
-    // right 
-    if (j + 1 < n && vis[i][j + 1] == 0 && a[i][j + 1] == 1) {
-      vis[i][j] = 1;
-      solve(i, j + 1, a, n, ans, move + 'R', vis);
-      vis[i][j] = 0;
-    }
-
-    // upward
-    if (i - 1 >= 0 && vis[i - 1][j] == 0 && a[i - 1][j] == 1) {
-      vis[i][j] = 1;
-      solve(i - 1, j, a, n, ans, move + 'U', vis);
-      vis[i][j] = 0;
-    }
-  }
-  public static ArrayList < String > findPath(int[][] m, int n) {
-    int vis[][] = new int[n][n];
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < n; j++) {
-        vis[i][j] = 0;
-      }
-    }
-
-    ArrayList < String > ans = new ArrayList < > ();
-    if (m[0][0] == 1) solve(0, 0, m, n, ans, "", vis);
-    return ans;
-  }
 }
